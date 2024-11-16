25430dbf5e963081c4532893974375bfec9a690e
michaelmnemonic <michaelmnemonic@posteo.eu>
2024-11-16 13:48:35 +0100

    Import common configuration

--- pluto.nix
+++ pluto.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   imports = [
     ./hardware/pluto.nix
+    ./_shared/common.nix
   ];
   system.autoUpgrade.flake = "github:michaelmnemonic/nixos";
   system.stateVersion = "24.05"; # Did you read the comment?
