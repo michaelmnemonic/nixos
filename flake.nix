8fb912a66e0bb37fd247fe7eab3ff42ff82d5ce0
michaelmnemonic <michaelmnemonic@posteo.eu>
2024-11-16 21:35:29 +0100

    Add home-manager

--- flake.nix
+++ flake.nix
@@ -4,6 +4,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     lanzaboote = {
       url = "github:nix-community/lanzaboote/v0.4.1";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -17,6 +22,7 @@
 
   outputs = {
     self,
+    home-manager,
     nixpkgs,
     lanzaboote,
     umu,
@@ -26,6 +32,12 @@
       modules = [
         lanzaboote.nixosModules.lanzaboote
         ./pluto.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.maik = import ./home.nix;
+        }
       ];
       specialArgs = {inherit umu;};
     };
