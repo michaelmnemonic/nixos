818eabeedb8367e88668a05dc9f7bb0b5f9e4eb5
michaelmnemonic <michaelmnemonic@posteo.eu>
2024-11-16 13:33:36 +0100

    Add hardware configuration for pluto

---
+++ hardware/pluto.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  modulesPath,
+  ...
+}: {
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "amdgpu"];
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = ["kvm-amd"];
+  boot.extraModulePackages = [];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/NIXOS";
+    fsType = "ext4";
+  };
+
+  boot.initrd.luks.devices."NIXOS" = {
+    device = "/dev/disk/by-partlabel/NIXOS";
+    allowDiscards = true;
+    bypassWorkqueues = true;
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/EFI";
+    fsType = "vfat";
+  };
+
+  swapDevices = [];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+}
