90b733ef2709e653aae2d753460d3956ba70f09d
michaelmnemonic <michaelmnemonic@posteo.eu>
2024-11-16 14:05:32 +0100

    Add customization of plasma desktop

--- pluto.nix
+++ pluto.nix
@@ -14,6 +14,9 @@
   # Use latest stable kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
+  # Boot splash screen
+  boot.plymouth.enable = true;
+
   # Use german keyboard layout
   services.xserver = {
     # set keymap
@@ -31,6 +34,65 @@
       compositor = "kwin";
     };
   };
+
+  # Use NetworkManager
+  networking.networkmanager.enable = true;
+
+  # Disable NetworkManager wait online
+  systemd.services."NetworkManager-wait-online".enable = false;
+
+  # Add inter, jetbrains-mono and noto fonts
+  fonts.packages = with pkgs; [
+    inter
+    jetbrains-mono
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+  ];
+
+  # List of system-wide packages
+  environment.systemPackages = with pkgs; [
+    (catppuccin-kde.override {
+      flavour = ["mocha" "latte"];
+      accents = ["rosewater"];
+    })
+    aspell
+    aspellDicts.de
+    aspellDicts.en
+    ffmpegthumbs
+    firefox
+    fooyin
+    gitMinimal
+    kdePackages.akonadi
+    kdePackages.akonadi-calendar
+    kdePackages.akonadi-contacts
+    kdePackages.akonadi-mime
+    kdePackages.akonadi-notes
+    kdePackages.akonadi-search
+    kdePackages.elisa
+    kdePackages.kdepim-addons
+    kdePackages.kdepim-runtime
+    kdePackages.kmail
+    kdePackages.kmail-account-wizard
+    kdePackages.ksshaskpass
+    kdePackages.merkuro
+    kdePackages.tokodon
+    libreoffice-qt
+    nfs-utils
+    syncthing
+    transmission_4-qt
+  ];
+
+  # Enable kdeconnect
+  programs.kdeconnect = {
+    enable = true;
+  };
+
+  # Enable dconf (needed for configuration of gtk themes under wayland)
+  programs.dconf.enable = true;
+
+  # Enable ssh-agent
+  programs.ssh.startAgent = true;
 
   system.autoUpgrade.flake = "github:michaelmnemonic/nixos";
   system.stateVersion = "24.05"; # Did you read the comment?
