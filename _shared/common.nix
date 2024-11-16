1215fdb8abebfbea78451c5ec5fc1f4137231f92
michaelmnemonic <michaelmnemonic@posteo.eu>
2024-11-16 13:53:46 +0100

    Add basic configuration

--- _shared/common.nix
+++ _shared/common.nix
@@ -1,8 +1,4 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
+{...}: {
   # Use systemd-boot as EFI boot loader.
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.editor = false;
@@ -19,4 +15,32 @@
     "loglevel=0"
     "vt.global_cursor_default=0"
   ];
+
+  # Use zram as swap
+  zramSwap = {
+    enable = true;
+    algorithm = "zstd";
+  };
+
+  # Setup time zone
+  time.timeZone = "Europe/Berlin";
+
+  # Use german internationalisation
+  i18n.defaultLocale = "de_DE.UTF-8";
+  console.keyMap = "de";
+
+  # Enable sude for user of group wheel
+  security.sudo.enable = true;
+  security.sudo.execWheelOnly = true;
+
+  # Enable flakes
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # Garbage collect nix store
+  nix.settings.auto-optimise-store = true;
+  nix.gc.automatic = true;
+  nix.gc.options = "--delete-older-than 7d";
+
+  # Never automatically reboot
+  system.autoUpgrade.allowReboot = false;
 }
--- pluto.nix
+++ pluto.nix
@@ -3,6 +3,12 @@
     ./hardware/pluto.nix
     ./_shared/common.nix
   ];
+
+  # Disable mitigations for performance
+  # FIXME: is this a good idea?
+  boot.kernelParams = [
+    "mitigations=off"
+  ];
   system.autoUpgrade.flake = "github:michaelmnemonic/nixos";
   system.stateVersion = "24.05"; # Did you read the comment?
 }
