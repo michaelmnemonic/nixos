bd9c65c59e8e60bd4d5eea2d9813cea96fb089d3
michaelmnemonic <michaelmnemonic@posteo.eu>
2024-11-30 21:55:04 +0100

    Switch to fscrypt backed home dir

--- users/maik.nix
+++ users/maik.nix
@@ -1,10 +1,9 @@
 {...}: {
-  security.pam.mount.enable = true;
+  security.pam.enableFscrypt = true;
   users.users.maik = {
     isNormalUser = true;
     description = "Maik Köhler";
     initialHashedPassword = "$y$j9T$BxQsIrZDobn4n7SRol8QE1$BNhg4USV5qCboQab8zQJex6BQCJN6rQiF4fDnXG/Mz6";
-    cryptHomeLuks = "/dev/disk/by-partlabel/MAIK";
     extraGroups = ["wheel"]; # Enable ‘sudo’ for the user.
     openssh.authorizedKeys.keys = [
     ];
