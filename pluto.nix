04775a000d73a393648d1cf61974e56ed68187f1
michaelmnemonic <michaelmnemonic@posteo.eu>
2024-12-01 19:03:14 +0100

    Enable flatpak

--- pluto.nix
+++ pluto.nix
@@ -134,6 +134,16 @@
     zed-editor
   ];
 
+  # Enable flatpak
+  services.flatpak.enable = true;
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
+
   # Enable user service for syncthing
   # systemd.user.services.syncthing.wantedBy = ["default.target"];
 
