b35a41937671080c621564629d5b38746bf67bd4
michaelmnemonic <michaelmnemonic@posteo.eu>
2024-12-31 23:20:20 +0100

    Cleanup flake

--- flake.nix
+++ flake.nix
@@ -3,51 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-
-    umu = {
-      url = "github:Open-Wine-Components/umu-launcher/?dir=packaging\/nix&submodules=1/1.1.4";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    agenix = {
-      url = "github:ryantm/agenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    autotag = {
-      url = "git+file:///var/lib/autotag";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = {
     self,
-    agenix,
     nixpkgs,
-    autotag,
-    umu,
   }: {
     nixosConfigurations = {
       pluto = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          agenix.nixosModules.default
           ./pluto.nix
         ];
         specialArgs = {
-          inherit umu;
-          inherit agenix;
-          inherit autotag;
         };
       };
       juno = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          agenix.nixosModules.default
           ./juno.nix
         ];
         specialArgs = {
-          inherit autotag;
         };
       };
     };
