127635007f95444a1bf8b22c3a9af524d8933496
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-01-10 23:44:56 +0100

    Add to kvm group

--- users/maik.nix
+++ users/maik.nix
@@ -4,7 +4,7 @@
     isNormalUser = true;
     description = "Maik Köhler";
     initialHashedPassword = "$y$j9T$BxQsIrZDobn4n7SRol8QE1$BNhg4USV5qCboQab8zQJex6BQCJN6rQiF4fDnXG/Mz6";
-    extraGroups = ["wheel"]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "kvm" ]; # Enable ‘sudo’ for the user.
     openssh.authorizedKeys.keys = [
     ];
   };
