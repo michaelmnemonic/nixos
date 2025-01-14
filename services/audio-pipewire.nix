4bfa997ae45c5c3084d2e8960a34de90ec45ef3b
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-01-14 18:21:35 +0100

    Explicitly set quantum

--- services/audio-pipewire.nix
+++ services/audio-pipewire.nix
@@ -23,9 +23,9 @@
           ## Properties for the DSP configuration.
           default.clock.rate          = 48000
           default.clock.allowed-rates = [ 44100 48000 88200 96000 ]
-          #default.clock.quantum       = 512
-          #default.clock.min-quantum   = 32
-          #default.clock.max-quantum   = 1024
+          default.clock.quantum       = 512
+          default.clock.min-quantum   = 32
+          default.clock.max-quantum   = 1024
           #default.clock.quantum-limit = 8192
           #default.video.width         = 640
           #default.video.height        = 480
