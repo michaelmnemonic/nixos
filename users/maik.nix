7642fc61116ab24be5098bc56bad64f213a32f09
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-01-27 10:53:52 +0100

    Add key on charon

--- users/maik.nix
+++ users/maik.nix
@@ -6,6 +6,7 @@
     initialHashedPassword = "$y$j9T$BxQsIrZDobn4n7SRol8QE1$BNhg4USV5qCboQab8zQJex6BQCJN6rQiF4fDnXG/Mz6";
     extraGroups = [ "wheel" "kvm" ]; # Enable ‘sudo’ for the user.
     openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICfbWSyDza2x5JhQW2oFHoDmO2FEqlrTHRKqe5f/GB2A maik@charon"
     ];
   };
 }
