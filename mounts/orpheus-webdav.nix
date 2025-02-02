a8e76c2c687a8dbe3131394a12d10bbaa474b567
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-02-02 01:33:30 +0100

    Mount orpheus via webdav

---
+++ mounts/orpheus-webdav.nix
@@ -0,0 +1,27 @@
+{...}: {
+
+  # Enable davfs
+  services.davfs2.enable = true;
+
+  # Auto mount orpheus
+  systemd.mounts = [
+    {
+      type = "davfs";
+      mountConfig = {
+        Options = "rw,user,uid=maik,noatime";
+        ForceUnmount = true;
+      };
+      what = "https://orpheus.42evy4oo6scnaepd.myfritz.net/";
+      where = "/mnt";
+    }
+  ];
+  systemd.automounts = [
+    {
+      wantedBy = ["multi-user.target"];
+      automountConfig = {
+        TimeoutIdleSec = "600";
+      };
+      where = "/mnt";
+    }
+  ];
+}
