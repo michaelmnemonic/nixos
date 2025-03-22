889b628d45012b7e2b51ccc525318f6f609dff97
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-03-22 15:42:23 +0100

    Improve grammar

--- modules/nixos/steam.nix
+++ modules/nixos/steam.nix
@@ -6,7 +6,7 @@
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    # prevent steam to access wifi passwords
+    # prevent steam from asking for wifi passwords
     package = pkgs.steam.override {extraProfile = "export DBUS_SYSTEM_BUS_ADDRESS=";};
   };
 }
