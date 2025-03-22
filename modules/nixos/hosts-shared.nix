0e230066cd19c63b62064789bf026d6f5666f1f0
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-03-22 15:33:39 +0100

    Mount orpheus on all hosts

--- modules/nixos/hosts-shared.nix
+++ modules/nixos/hosts-shared.nix
@@ -44,6 +44,30 @@
   # Enable fscrypted home directories
   security.pam.enableFscrypt = true;
 
+  # Auto mount orpheus:
+  services.rpcbind.enable = true; # needed for NFS
+  systemd.mounts = [
+    {
+      type = "nfs";
+      mountConfig = {
+        Options = "noatime";
+        ForceUnmount = true;
+        TimeoutSec = "5s";
+      };
+      what = "192.168.178.30:";
+      where = "/mnt";
+    }
+  ];
+  systemd.automounts = [
+    {
+      wantedBy = ["multi-user.target"];
+      automountConfig = {
+        TimeoutIdleSec = "600";
+      };
+      where = "/mnt";
+    }
+  ];
+
   # Enable flakes
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
