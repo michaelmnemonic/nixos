c9e1204a3ccb418b31f32bcd63cd38e787d2aa91
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-03-29 17:16:08 +0100

    Add shared home config for pluto and charon

---
+++ modules/home/maik.nix
@@ -0,0 +1,41 @@
+{pkgs, ...}:{
+    # Enable syncthing
+    services.syncthing.enable = true;
+
+    # Manage systemd user services
+    systemd.user.enable = true;
+
+    # Install user packages
+    home.packages = with pkgs; [
+        zed-editor
+    ];
+
+    # Dotfiles
+    home.file = {
+        # mpv
+        ".config/mpv/mpv.conf".text = ''
+            vo=gpu-next
+            gpu-context=waylandvk
+        '';
+        ".config/mpv/input.conf".text = ''
+            WHEEL_UP      seek  10
+            WHEEL_DOWN    seek -10
+        '';
+
+        # kwin window rules
+        ".config/kwinrulesrc".text = ''
+            [General]
+            count=1
+            rules=dd064355-b15d-4681-a245-b7b757480f63
+
+            [dd064355-b15d-4681-a245-b7b757480f63]
+            Description=Dark mode für mpv
+            decocolor=BreezeDark
+            decocolorrule=2
+            wmclass=mpv
+            wmclassmatch=1
+        '';
+    };
+
+    home.stateVersion = "24.11";
+}
