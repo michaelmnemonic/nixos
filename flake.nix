0665d878756b60d528f829d1e884e24dc175803a
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-04-02 23:01:30 +0200

    Improve readability through some convinient variables

--- flake.nix
+++ flake.nix
@@ -2,63 +2,61 @@
   description = "Nix configuration for several private host systems";
 
   inputs = {
-      nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-24.11";
 
-      nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-      nixos-x13s = {
-        url = "github:michaelmnemonic/x13s-nixos";
-        inputs.nixpkgs.follows = "nixpkgs-unstable";
+    nixos-x13s = {
+      url = "github:michaelmnemonic/x13s-nixos";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs-stable,
+    nixpkgs-unstable,
+    nixos-x13s,
+  }: let
+    # Define 'forAllSystems' for properties that shall be build for x86_64 *and* aarch64
+    systems = ["x86_64-linux" "aarch64-linux"];
+    forAllSystems = nixpkgs-unstable.lib.genAttrs systems;
+  in {
+    nixosConfigurations = {
+      pluto = nixpkgs-stable.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/pluto.nix
+        ];
+        specialArgs = {
+        };
+      };
+      juno = nixpkgs-stable.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/juno.nix
+        ];
+        specialArgs = {
+        };
+      };
+      charon = nixpkgs-unstable.lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [
+          nixos-x13s.nixosModules.default
+          ./hosts/charon.nix
+        ];
+        specialArgs = {
+          inherit nixos-x13s;
+        };
       };
     };
-
-    outputs = {
-      self,
-      nixpkgs-stable,
-      nixpkgs-unstable,
-      nixos-x13s,
-    }: {
-      nixosConfigurations = {
-        pluto = nixpkgs-stable.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./hosts/pluto.nix
-          ];
-          specialArgs = {
-          };
-        };
-        juno = nixpkgs-stable.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./hosts/juno.nix
-          ];
-          specialArgs = {
-          };
-        };
-        charon = nixpkgs-unstable.lib.nixosSystem {
-          system = "aarch64-linux";
-          modules = [
-            nixos-x13s.nixosModules.default
-            ./hosts/charon.nix
-          ];
-          specialArgs = {
-            inherit nixos-x13s;
-          };
-        };
-      };
-      devShell.x86_64-linux = nixpkgs-unstable.legacyPackages.x86_64-linux.pkgs.mkShell {
-        buildInputs = with nixpkgs-unstable.legacyPackages.x86_64-linux.pkgs; [
+    devShell = forAllSystems (system:
+      nixpkgs-unstable.legacyPackages.${system}.pkgs.mkShell {
+        buildInputs = with nixpkgs-unstable.legacyPackages.${system}.pkgs; [
           gitMinimal
           nil
           alejandra
         ];
-      };
-      devShell.aarch64-linux = nixpkgs-unstable.legacyPackages.aarch64-linux.pkgs.mkShell {
-        buildInputs = with nixpkgs-unstable.legacyPackages.aarch64-linux.pkgs; [
-          gitMinimal
-          nil
-          alejandra
-        ];
-      };
-    };
-  }
+      });
+  };
+}
