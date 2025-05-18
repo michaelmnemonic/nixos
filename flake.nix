c95bbaa359babc9cfdf82e4d56cf2b59a62b66b5
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-05-18 22:04:18 +0200

    Make juno follow unstable as well

--- flake.nix
+++ flake.nix
@@ -2,28 +2,25 @@
   description = "Nix configuration for several private host systems";
 
   inputs = {
-    nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-24.11";
-
-    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
     nixos-x13s = {
       url = "github:michaelmnemonic/x13s-nixos";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = {
     self,
-    nixpkgs-stable,
-    nixpkgs-unstable,
+    nixpkgs,
     nixos-x13s,
   }: let
     # Define 'forAllSystems' for properties that shall be build for x86_64 *and* aarch64
     systems = ["x86_64-linux" "aarch64-linux"];
-    forAllSystems = nixpkgs-unstable.lib.genAttrs systems;
+    forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
     nixosConfigurations = {
-      pluto = nixpkgs-unstable.lib.nixosSystem {
+      pluto = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./hosts/pluto.nix
@@ -31,7 +28,7 @@
         specialArgs = {
         };
       };
-      juno = nixpkgs-stable.lib.nixosSystem {
+      juno = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./hosts/juno.nix
@@ -39,7 +36,7 @@
         specialArgs = {
         };
       };
-      charon = nixpkgs-unstable.lib.nixosSystem {
+      charon = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
         modules = [
           nixos-x13s.nixosModules.default
@@ -52,7 +49,7 @@
     };
     devShell = forAllSystems (system:
       let
-        pkgs = nixpkgs-unstable.legacyPackages.${system}.pkgs;
+        pkgs = nixpkgs.legacyPackages.${system}.pkgs;
       in
         pkgs.mkShell {
           buildInputs = with pkgs; [
