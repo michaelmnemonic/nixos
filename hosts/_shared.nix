b6b9ee5e50a0fb39ed211e855fd68d85010f6947
michalemnemonic <michalemnemonic@posteo.eu>
2025-05-29 20:36:44 +0200

    Limit number of generations

--- hosts/_shared.nix
+++ hosts/_shared.nix
@@ -11,6 +11,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.editor = false;
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.timeout = 0;
 
   # Kernel command line
