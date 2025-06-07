4ded77de8dbc619313caa532cb012bd31dbcf381
michalemnemonic <michalemnemonic@posteo.eu>
2025-06-07 15:35:26 +0200

    Add more subid / subgid for maik

--- users/maik.nix
+++ users/maik.nix
@@ -10,6 +10,18 @@ in {
     initialHashedPassword = "$y$j9T$BxQsIrZDobn4n7SRol8QE1$BNhg4USV5qCboQab8zQJex6BQCJN6rQiF4fDnXG/Mz6";
     extraGroups = ["wheel" "kvm" "docker"]; # Enable ‘sudo’ for the user.
     openssh.authorizedKeys.keys = ssh_keys;
-  };
+    subUidRanges = [
+        {
+        count = 65536;
+        startUid = 1000000;
+        }
+    ];
+  subGidRanges = [
+      {
+      count = 65536;
+      startGid = 1000000;
+      }
+  ];
+};
   users.users.root.openssh.authorizedKeys.keys = ssh_keys;
 }
