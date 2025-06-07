bfa69fc6af9f9cc7a5fdda1d2634ba95bab6ef07
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-06-07 18:01:14 +0200

    Format

--- capabilities/vscode.nix
+++ capabilities/vscode.nix
@@ -1,28 +1,26 @@
-{pkgs, ...}:
-let
+{pkgs, ...}: let
   inherit (pkgs) vscode-extensions vscode-utils vscode-with-extensions;
 
   continue = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-          mktplcRef = {
-            name = "continue";
-            publisher = "Continue";
-            version = "1.1.40";
-            sha256 = "sha256-P4rhoj4Juag7cfB9Ca8eRmHRA10Rb4f7y5bNGgVZt+E=";
-            arch = "linux-x64";
-          };
-          # Patch obtained from: https://github.com/continuedev/continue/issues/821
-          nativeBuildInputs = [pkgs.autoPatchelfHook];
-          buildInputs = [pkgs.stdenv.cc.cc.lib];
-        };
+    mktplcRef = {
+      name = "continue";
+      publisher = "Continue";
+      version = "1.1.40";
+      sha256 = "sha256-P4rhoj4Juag7cfB9Ca8eRmHRA10Rb4f7y5bNGgVZt+E=";
+      arch = "linux-x64";
+    };
+    # Patch obtained from: https://github.com/continuedev/continue/issues/821
+    nativeBuildInputs = [pkgs.autoPatchelfHook];
+    buildInputs = [pkgs.stdenv.cc.cc.lib];
+  };
 
   vscode = vscode-with-extensions.override {
-    vscodeExtensions = with vscode-extensions;
-      [
-        continue
-        jnoortheen.nix-ide
-        mkhl.direnv
-        ms-vscode-remote.remote-ssh
-      ];
+    vscodeExtensions = with vscode-extensions; [
+      continue
+      jnoortheen.nix-ide
+      mkhl.direnv
+      ms-vscode-remote.remote-ssh
+    ];
   };
 in {
   environment.systemPackages = with pkgs; [
