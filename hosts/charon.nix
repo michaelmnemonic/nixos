d508253c42806a9998a08156246b527f9c911b1f
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-06-22 22:04:04 +0200

    Drop anytype

--- hosts/charon.nix
+++ hosts/charon.nix
@@ -121,7 +121,6 @@
 
   # List of system-wide packages
   environment.systemPackages = with pkgs; [
-    anytype
     (pkgs.kodi.withPackages (kodiPkgs:
       with pkgs; [
         python312Packages.pillow
