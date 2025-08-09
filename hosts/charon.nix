6256e5ea82e73be7fd273af0aca407b04bfd15e7
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-08-09 19:21:56 +0200

    Use different adress

--- hosts/charon.nix
+++ hosts/charon.nix
@@ -90,7 +90,7 @@
           publicKey = "b2D3/C+3yCuzNGW4zYZ8vUMFIO1MUeAp8DoVfjbv3QQ=";
           presharedKeyFile = config.age.secrets."orpheus_charon.psk".path;
           allowedIPs = ["10.0.0.0/24"];
-          endpoint = "orpheus.42evy4oo6scnaepd.myfritz.net:51820";
+          endpoint = "maikkoehler.eu:51820";
           dynamicEndpointRefreshSeconds = 600;
         }
       ];
