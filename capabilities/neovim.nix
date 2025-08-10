6b56099d668c0c2bbc6fa96430e9e4a16139e4ef
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-08-11 01:45:28 +0200

    Fix imports

--- capabilities/neovim.nix
+++ capabilities/neovim.nix
@@ -1,16 +1,12 @@
 {pkgs, ...}: {
   programs.neovim = {
     enable = true;
-    configure = {
-      customRC = ''
-        set number
-        set cc=80
-      '';
+    configure= {
       packages.myVimPackage = with pkgs.vimPlugins; {
         start = [
           ctrlp
-          vim-nix
           nvim-lspconfig
+          vim-nix
         ];
       };
     };
