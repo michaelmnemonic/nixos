dabe0cd8e062f1dae8d9d40cf122e01b4cfab495
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-08-10 17:30:52 +0200

    Add ssh for juno

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,8 @@
 let
   maik_charon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIILpZoy2khZNpMO05fNXVIB/6OWqarzgv7OOubj+JWgH";
   maik_pluto = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICfEOsO8EXO+oW8sisd+JHrT6FrvnuY+1xAVPEz5Prhm";
-  users = [maik_charon maik_pluto];
+  maik_juno = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMdpU8lQTPUVpDxIQ8cyqBFTfGfxBNkNtIxfNa8NHger maik@juno";
+  users = [maik_charon maik_pluto maik_juno];
 
   charon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPoesBYo+KX7Xn9svwOt3qqO6hMHwlDN+vuhiSvFFSpD";
   pluto = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEFn+etfEQXxsj9be8x40LrBqgVcFtlxVCqbLtCbeRuK";
