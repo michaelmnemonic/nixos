2b624d6cd58fcb27815fab152f8206a4f4709fc1
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-08-10 14:04:11 +0200

    Drop automatic printer config

--- capabilities/printing.nix
+++ capabilities/printing.nix
@@ -4,20 +4,4 @@
     enable = true;
     drivers = [pkgs.hplip];
   };
-  hardware.printers = {
-    # Setup default printers
-    ensurePrinters = [
-      {
-        name = "Laserdrucker";
-        location = "Flur";
-        deviceUri = "ipp://B432-820DB6/ipp";
-        model = "everywhere";
-        ppdOptions = {
-          PageSize = "A4";
-          Duplex = "DuplexNoTumble";
-        };
-      }
-    ];
-    ensureDefaultPrinter = "Laserdrucker";
-  };
 }
