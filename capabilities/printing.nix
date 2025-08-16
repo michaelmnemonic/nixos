2974ba2e87c378493d47edee332c10ffbbb5e3ae
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-08-16 13:40:29 +0200

    Enable printer auto discovery

--- capabilities/printing.nix
+++ capabilities/printing.nix
@@ -1,6 +1,17 @@
-{...}: {
+{ pkgs, ...}: {
   # Enable printing
   services.printing = {
     enable = true;
+    drivers = with pkgs; [
+        cups-filters
+        cups-browsed
+      ];
+  };
+
+  # Enable auto discovery
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
   };
 }
