c484efdb326f1b85d2479e7e5a4065ae53b2c692
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-08-24 00:02:56 +0200

    Revert "Reapply "Changed fan setup""
    This reverts commit b9fc4dcee033d5bf898895064ee8f0fd872d9813.

--- hosts/pluto.nix
+++ hosts/pluto.nix
@@ -322,50 +322,35 @@
   # Fan control
   environment.etc."fan2go/fan2go.yaml".text = ''
     fans:
-      - id: front_bottom
+      - id: side
         hwmon:
-          platform: nct6798-isa-0290
+          platform: nct6792-isa-0290
           index: 1
         neverStop: true
-        curve: front_curve
-      - id: front_top
-        hwmon:
-          platform: nct6798-isa-0290
-          index: 5
-        neverStop: true
-        curve: front_curve
-      - id: top
-        hwmon:
-          platform: nct6798-isa-0290
-          index: 7
-        neverStop: true
-        curve: front_curve
-      - id: back
-        hwmon:
-          platform: nct6798-isa-0290
-          index: 6
-        neverStop: true
-        curve: cpu_curve
+        minPwm: 80
+        curve: side_curve
       - id: cpu
         hwmon:
-          platform: nct6798-isa-0290
+          platform: nct6792-isa-0290
           index: 2
         neverStop: true
         curve: cpu_curve
-      - id: gpu
+      - id: bottom
         hwmon:
-          platform: nct6798-isa-0290
-          index: 4
+          platform: nct6792-isa-0290
+          index: 3
         neverStop: true
+        minPwm: 80
         curve: gpu_curve
+        controlAlgorithm: direct
     sensors:
       - id: gpu_edge
         hwmon:
-          platform: amdgpu-pci-0b00
+          platform: amdgpu-pci-0800
           index: 1
       - id: gpu_mem
         hwmon:
-          platform: amdgpu-pci-0b00
+          platform: amdgpu-pci-0800
           index: 3
       - id: cpu_tctl
         hwmon:
@@ -373,20 +358,19 @@
           index: 1
     curves:
       - id: gpu_edge_curve
-        linear:
+        pid:
           sensor: gpu_edge
-          steps:
-            - '50': 80
-            - '60': 100
-            - '70': 130
+          setPoint: 70
+          p: -0.05
+          i: -0.005
+          d: -0.005
       - id: gpu_mem_curve
-        linear:
+        pid:
           sensor: gpu_mem
-          steps:
-            - '70': 80
-            - '90': 100
-            - '100': 150
-            - '100': 155
+          setPoint: 100
+          p: -0.05
+          i: -0.005
+          d: -0.005
       - id: gpu_curve
         function:
           type: maximum
@@ -397,30 +381,17 @@
         linear:
           sensor: cpu_tctl
           steps:
-            - '50': 80
-            - '60': 100
-            - '70': 130
-      - id: front_curve
+            - 50: 80
+            - 60: 100
+            - 70: 130
+      - id: side_curve
         function:
           type: maximum
           curves:
-            - front_cpu_curve
-            - front_gpu_curve
-      - id: front_cpu_curve
-        linear:
-          sensor: cpu_tctl
-          steps:
-            - '50': 50
-            - '60': 70
-            - '70': 110
-      - id: front_gpu_curve
-        linear:
-          sensor: gpu_edge
-          steps:
-            - '55': 50
-            - '60': 110
-            - '65': 150
+            - cpu_curve
+            - gpu_curve
   '';
+
   ############
   # Programs #
   ############
