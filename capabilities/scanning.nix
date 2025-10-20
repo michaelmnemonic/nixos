9b3bd81b291306d22b605b55299bc3909207e21b
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-10-20 22:33:45 +0200

    Enable network scan

--- capabilities/scanning.nix
+++ capabilities/scanning.nix
@@ -3,6 +3,11 @@
   hardware.sane = {
     enable = true;
     extraBackends = [pkgs.sane-airscan];
+    openFirewall = true;
+    netConf = "
+10.0.0.1
+192.168.178.30
+    ";
   };
   services.saned.enable = true;
 }
