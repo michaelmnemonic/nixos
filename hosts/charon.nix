aa364544742847982282f498a1b791e936fd60f0
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-10-26 22:48:48 +0100

    Switch to mainline kernel

--- hosts/charon.nix
+++ hosts/charon.nix
@@ -36,7 +36,7 @@
     enable = true;
     wifiMac = "F4:A8:0D:F5:5D:BC";
     bluetoothMac = "F4:A8:0D:30:9D:8B";
-    kernel = "jhovold";
+    kernel = "mainline";
   };
 
   # Allow unfree software
