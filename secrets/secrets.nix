e65ecf64ebb9803b3190e1af56fcd6206bae5e71
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-12-03 00:05:45 +0100

    Rotate keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  maik_charon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHNHxKH9KqMxSbslkgzBCdMwqrlDzM/+TtlmHuuQJYTT";
+  maik_charon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAkpHhxgnv0LKZE0evDYtOqxlWKdIoycoknSJWrJ7bUX";
   maik_pluto = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDPW7zg350UKZ73FKYvcY6OO0M7H23/Z84QF8pSXg1pl";
   maik_juno = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMdpU8lQTPUVpDxIQ8cyqBFTfGfxBNkNtIxfNa8NHger maik@juno";
   users = [
@@ -8,7 +8,7 @@ let
     maik_juno
   ];
 
-  charon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBf2HR9GXkOb45zyZjlpsPdkagPXpwMYrwPC8h4L3LUZ";
+  charon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILNYvp3EZuiCy9ttnBKeV2/+XWblB0hmHwsAKOkTL/Bw";
   pluto = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILCU5RAqeCO4gcln0Cv7QWREYGnJkJ+QwZPv9MfVzL0B";
   juno = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBgX0SJ0x/vYTxM+q8AS0fGNJA8Kh4CVf6Scvtf1UGB0";
   flore = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGa1hvRBpAFaN2zU1zQXt3l0/CvHypQ4WGhcsG8GB0sf";
