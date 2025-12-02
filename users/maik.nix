43be726d676375140149f398bc2a0f30320f9572
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-12-02 23:24:19 +0100

    Rotate keys

--- users/maik.nix
+++ users/maik.nix
@@ -1,16 +1,23 @@
-{...}: let
+{ ... }:
+let
   ssh_keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICfEOsO8EXO+oW8sisd+JHrT6FrvnuY+1xAVPEz5Prhm maik@pluto"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHNHxKH9KqMxSbslkgzBCdMwqrlDzM/+TtlmHuuQJYTT maik@charon"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAkpHhxgnv0LKZE0evDYtOqxlWKdIoycoknSJWrJ7bUX maik@charon"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMdpU8lQTPUVpDxIQ8cyqBFTfGfxBNkNtIxfNa8NHger maik@juno"
   ];
-in {
+in
+{
   users.users.maik = {
     isNormalUser = true;
     description = "Maik Köhler";
     uid = 1000;
     initialHashedPassword = "$y$j9T$BxQsIrZDobn4n7SRol8QE1$BNhg4USV5qCboQab8zQJex6BQCJN6rQiF4fDnXG/Mz6";
-    extraGroups = ["wheel" "kvm" "docker" "networkmanager"]; # Enable ‘sudo’ for the user.
+    extraGroups = [
+      "wheel"
+      "kvm"
+      "docker"
+      "networkmanager"
+    ]; # Enable ‘sudo’ for the user.
     openssh.authorizedKeys.keys = ssh_keys;
     subUidRanges = [
       {
