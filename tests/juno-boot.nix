fb216c388bf77bd893e2d76516baca9fbe5139bc
michaelmnemonic <michaelmnemonic@posteo.eu>
2026-01-21 23:16:21 +0100

    Format.

--- tests/juno-boot.nix
+++ tests/juno-boot.nix
@@ -1,36 +1,33 @@
-{ agenix }:
-{
+{agenix}: {
   name = "juno-boot";
 
-  nodes.machine =
-    { lib, ... }:
-    {
-      imports = [
-        ../hosts/juno.nix
-        agenix.nixosModules.default
-      ];
+  nodes.machine = {lib, ...}: {
+    imports = [
+      ../hosts/juno.nix
+      agenix.nixosModules.default
+    ];
 
-      # Increase memory and cores for the VM
-      virtualisation.memorySize = 2048;
-      virtualisation.cores = 2;
+    # Increase memory and cores for the VM
+    virtualisation.memorySize = 2048;
+    virtualisation.cores = 2;
 
-      # Override filesystem configuration to be compatible with the test VM
-      fileSystems = lib.mkForce {
-        "/" = {
-          device = "/dev/vda";
-          fsType = "ext4";
-        };
+    # Override filesystem configuration to be compatible with the test VM
+    fileSystems = lib.mkForce {
+      "/" = {
+        device = "/dev/vda";
+        fsType = "ext4";
       };
-
-      # Disable LUKS encryption setup expected on real hardware
-      boot.initrd.luks.devices = lib.mkForce { };
-
-      # Prevent conflict with the externally created nixpkgs instance
-      nixpkgs.config = lib.mkForce { };
-
-      # Resolve conflict with test-instrumentation
-      boot.consoleLogLevel = lib.mkForce 0;
     };
+
+    # Disable LUKS encryption setup expected on real hardware
+    boot.initrd.luks.devices = lib.mkForce {};
+
+    # Prevent conflict with the externally created nixpkgs instance
+    nixpkgs.config = lib.mkForce {};
+
+    # Resolve conflict with test-instrumentation
+    boot.consoleLogLevel = lib.mkForce 0;
+  };
 
   testScript = ''
     machine.wait_for_unit("multi-user.target")
--- tests/pluto-boot.nix
+++ tests/pluto-boot.nix
@@ -1,40 +1,37 @@
-{ agenix }:
-{
+{agenix}: {
   name = "pluto-boot";
 
-  nodes.machine =
-    { lib, ... }:
-    {
-      imports = [
-        ../hosts/pluto.nix
-        agenix.nixosModules.default
-      ];
+  nodes.machine = {lib, ...}: {
+    imports = [
+      ../hosts/pluto.nix
+      agenix.nixosModules.default
+    ];
 
-      # Increase memory and cores for the VM
-      virtualisation.memorySize = 2048;
-      virtualisation.cores = 2;
+    # Increase memory and cores for the VM
+    virtualisation.memorySize = 2048;
+    virtualisation.cores = 2;
 
-      # Override filesystem configuration to be compatible with the test VM
-      # The real hardware configuration expects specific partitions and Btrfs subvolumes
-      fileSystems = lib.mkForce {
-        "/" = {
-          device = "/dev/vda";
-          fsType = "ext4";
-        };
+    # Override filesystem configuration to be compatible with the test VM
+    # The real hardware configuration expects specific partitions and Btrfs subvolumes
+    fileSystems = lib.mkForce {
+      "/" = {
+        device = "/dev/vda";
+        fsType = "ext4";
       };
-
-      # Disable LUKS encryption setup expected on real hardware
-      boot.initrd.luks.devices = lib.mkForce { };
-
-      # Disable fan2go service as hardware sensors are not available in VM
-      systemd.services.fan2go.enable = lib.mkForce false;
-
-      # Resolve conflict with test-instrumentation
-      boot.consoleLogLevel = lib.mkForce 0;
-
-      # Prevent conflict with the externally created nixpkgs instance
-      nixpkgs.config = lib.mkForce { };
     };
+
+    # Disable LUKS encryption setup expected on real hardware
+    boot.initrd.luks.devices = lib.mkForce {};
+
+    # Disable fan2go service as hardware sensors are not available in VM
+    systemd.services.fan2go.enable = lib.mkForce false;
+
+    # Resolve conflict with test-instrumentation
+    boot.consoleLogLevel = lib.mkForce 0;
+
+    # Prevent conflict with the externally created nixpkgs instance
+    nixpkgs.config = lib.mkForce {};
+  };
 
   testScript = ''
     machine.wait_for_unit("multi-user.target")
