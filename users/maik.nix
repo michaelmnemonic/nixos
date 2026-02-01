0ea52e08414e1fe0f2202f4ef7669f55edcc1dc4
michaelmnemonic <michaelmnemonic@posteo.eu>
2026-02-01 22:56:48 +0100

    Drop second name

--- users/maik.nix
+++ users/maik.nix
@@ -7,7 +7,7 @@
 in {
   users.users.maik = {
     isNormalUser = true;
-    description = "Maik Köhler";
+    description = "Maik";
     uid = 1000;
     initialHashedPassword = "$y$j9T$BxQsIrZDobn4n7SRol8QE1$BNhg4USV5qCboQab8zQJex6BQCJN6rQiF4fDnXG/Mz6";
     extraGroups = [
