1e7cb249eec596ad46352189ac0f3e7774047e2b
michaelmnemonic <michaelmnemonic@posteo.eu>
2026-04-12 14:08:52 +0200

    Patch plasma

--- gui/plasma.nix
+++ gui/plasma.nix
@@ -67,4 +67,23 @@ in {
 
   # Use ksshaskpass for ssh
   programs.ssh.askPassword = "${pkgs.kdePackages.ksshaskpass}/bin/ksshaskpass";
+
+  # Customize kde plasma
+  nixpkgs.overlays = [
+    (final: prev: {
+      kdePackages = prev.kdePackages.overrideScope (sfinal: sprev: {
+        # smaller systemtray icons with more spacing
+        # FIXME: this compiles plasma-workspace just to patch qml script
+        plasma-workspace = sprev.plasma-workspace.overrideAttrs (oldAttrs: {
+          patches =
+            oldAttrs.patches
+            ++ [
+              ../patches/0001-plasma-workspaces-systemtray-icon-sizes.patch
+              ../patches/0002-plasma-workspaces-lockout-icon-sizes.patch
+              ../patches/0003-plasma-workspaces-lockout-icon-lock-prompt.patch
+            ];
+        });
+      });
+    })
+  ];
 }
