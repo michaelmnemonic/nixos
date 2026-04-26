9b3e5feab0af22018f806239883df3c97bbf4555
michaelmnemonic <michaelmnemonic@posteo.eu>
2026-04-26 23:18:30 +0200

    Reapply "Switch root to f2fs"
    This reverts commit 7ab2fe758cc1f88bc5c9607fc15a10406d9560a0.

--- hardware/charon.nix
+++ hardware/charon.nix
@@ -48,8 +48,14 @@
   # Filesystems
   fileSystems."/" = {
     device = "/dev/disk/by-label/NIXOS";
-    fsType = "btrfs";
-    options = ["subvol=@" "compress=zstd:1"];
+    fsType = "f2fs";
+    options = [
+      "compress_algorithm=zstd:1"
+      "compress_chksum"
+      "atgc"
+      "gc_merge"
+      "lazytime"
+    ];
   };
 
   fileSystems."/boot" = {
