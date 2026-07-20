67a784d97cd168501215d30cd91f8fae20c08ba3
michaelmnemonic <michaelmnemonic@posteo.eu>
2026-07-20 20:09:31 +0200

    Switch to unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nix configuration for several private host systems";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-26.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
     nixos-x13s = {
       url = "github:michaelmnemonic/x13s-nixos";
