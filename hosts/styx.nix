c253edcecc3bae08829b6bb8b0380cd022740d79
michaelmnemonic <michaelmnemonic@posteo.eu>
2026-08-12 20:04:31 +0200

    CPU assignement handled by lpmd
    The assingement of cpu cores is now handled
    by intel-lpmd so we do not need systemd to
    do this anymore.

--- hosts/styx.nix
+++ hosts/styx.nix
@@ -303,24 +303,6 @@
     };
   };
 
-  # Restrict system-level daemons to efficency cores after start-up
-  systemd.slices."system".sliceConfig = {
-    AllowedCPUs = "4-7";
-    StartupAllowedCPUs = "";
-  };
-
-  # Restrict user session components to efficency cores after start-up
-  systemd.user.slices."session".sliceConfig = {
-    AllowedCPUs = "4-7";
-    StartupAllowedCPUs = "";
-  };
-
-  # Restrict user background tasks to efficency cores after start-up
-  systemd.user.slices."background".sliceConfig = {
-    AllowedCPUs = "4-7";
-    StartupAllowedCPUs = "";
-  };
-
   services.intel-lpmd = {
     enable = true;
     config.custom = {
